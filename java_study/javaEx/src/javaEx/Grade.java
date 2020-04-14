053af7051ca819e46dea401a6ff5efaa0c8f07e7
clownstudy <45422375+clownstudy@users.noreply.github.com>
2020-04-14 12:06:23 +0900

    클래스 생성 연습2 학점 계산기

---
+++ java_study/javaEx/src/javaEx/Grade.java
@@ -0,0 +1,32 @@
+package javaEx;
+
+public class Grade {
+// 성적을 출력하는 클래스
+// 성적을 입력하면 입력 받아서, 해당 학점을 출력하는 클래스
+//	100~90 : A
+// 89~80 : B  
+// 79~70 : C  
+// 69~60 : D
+// 69 이하 : F
+	
+	public String gradeMethod1(int score) {
+		// 들어온 값(score)에 따라서 학점을 리턴
+		String grade="";
+		
+		if(score >= 101) {
+			return "Error";
+		}
+		if(score <= 100 && score >= 90)
+			grade = "A"; 
+		if(score <= 89 && score >= 80)
+			grade = "B"; 
+		if(score <= 79 && score >= 70)
+			grade = "C"; 
+		if(score <= 69 && score >= 60)
+			grade = "D"; 
+		if(score <= 59)
+			grade = "F"; 
+		
+		return grade;		
+	}	
+}
