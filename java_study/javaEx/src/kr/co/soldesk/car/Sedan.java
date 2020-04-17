38b8840555bcdc907911206ea5bd2dee3cb6ff97
clownstudy <45422375+clownstudy@users.noreply.github.com>
2020-04-17 12:08:09 +0900

    override, extends, abstract
    오버라이드 시, @override로 재정의를 해주어야한다.

---
+++ java_study/javaEx/src/kr/co/soldesk/car/Sedan.java
@@ -0,0 +1,36 @@
+package kr.co.soldesk.car;
+
+public abstract class Sedan extends Car {
+	// 추상메소드는 반드시 하위클래스에서 구현해야함.
+	// 오버라이드 상위클래스의 메소드를 구현하는 행위
+	public Sedan() {
+
+	}
+
+	@Override
+	public void start() {
+		System.out.println("시동을 걸다");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("시동을 끄다");
+
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("기어를 바꾼다.");
+		System.out.println("전진");
+
+	}
+
+	@Override
+	public void redrive() {
+		System.out.println("기어를 바꾼다.");
+		System.out.println("경고음이 울린다.");
+		System.out.println("후진");
+
+	}
+
+}
