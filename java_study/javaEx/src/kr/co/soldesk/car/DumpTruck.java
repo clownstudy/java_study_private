8417e76101381d9ebcaa3e7e7df64d106ca0c17e
clownstudy <45422375+clownstudy@users.noreply.github.com>
2020-04-17 12:08:32 +0900

    override abstract extends

---
+++ java_study/javaEx/src/kr/co/soldesk/car/DumpTruck.java
@@ -0,0 +1,40 @@
+package kr.co.soldesk.car;
+
+public class DumpTruck extends Car {
+// 반드시 @Override 재정의를 해줘야함.
+	public DumpTruck() {
+	}
+	
+	public void dumpUp() {
+		System.out.println("적재칸을 올린다.");
+		
+	}
+	public void dumpDown() {
+		System.out.println("적재칸을 내린다.");
+		
+	}
+	
+	@Override
+	public void start() {
+		System.out.println("시동을 건다");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("시동을 끈다");
+		
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("기어를 바꾼다.");
+		System.out.println("안전하게 전진한다.");
+	}
+
+	@Override // 재정의
+	public void redrive() {
+		System.out.println("기어를 바꾼다.");
+		System.out.println("경고음이 울린다.");
+		System.out.println("후방을 주시하며 후진한다.");
+	}
+}
