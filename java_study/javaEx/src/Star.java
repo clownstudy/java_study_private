1d045064e89030545a526d67bfbe0f9aff98c165
clownstudy <45422375+clownstudy@users.noreply.github.com>
2020-04-20 11:37:40 +0900

    source 추가

---
+++ java_study/javaEx/src/Star.java
@@ -0,0 +1,19 @@
+
+public class Star {
+
+	public Star() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i+=2){
+			for(int j=0;j<i;j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+		
+	}
+
+}
